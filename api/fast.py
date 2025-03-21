4be42a010c7b15e6627da999cdb16be2e4caa9fa
Erika Azevedo <erikaa.chang@gmail.com>
2025-03-21 14:14:31 -0300

    modifications to the API so it can get an image for prediction

--- api/fast.py
+++ api/fast.py
@@ -1,7 +1,11 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from utils import prepara_data, load_model
+from utils import prepare_image, load_model
+from PIL import Image
+import io
+
 
 # Receberia a imagem
 # Prepararia ela (size, normalization, tensor)
@@ -20,17 +24,22 @@ app.add_middleware(
 )
 
 # http://127.0.0.1:8000/predict?image
-@app.get("/predict")
-def predict(image):
-    """
-    Make a single course prediction.
-    """
-    X = prepare_data(image)
-    
-    model = load_model()
-    result = dict(classification=model.predict(X))
+@app.post("/predict")
+async def predict(img: UploadFile = File(...)):
+    try:
+        # Ler e converter a imagem para um formato utilizável
+        contents = await img.read()
+        image = Image.open(io.BytesIO(contents))
 
-    return result
+        X = prepare_image(image, 256, 256)
+
+        model = load_model()
+        result = dict(classification=model.predict(X))
+
+        return JSONResponse(content=result, status_code=200)
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 @app.get("/")
 def root():
--- api/utils.py
+++ api/utils.py
@@ -1,24 +1,29 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras import layers
 
-def prepare_image(image):
+def prepare_image(image, height, width):
     """
     Prepare image datasets from directories
-    
+
     Args:
         Image
-        
+
     Returns:
-        Tensor
+        Numpy Array
     """
-    # Resize image
-    resized_image = 
-    
-    # Normalize
-    test_image = resized_image / 255
-    
-    # Optimize for performance
-    AUTOTUNE = tf.data.AUTOTUNE
-    test_image = test_image.prefetch(buffer_size=AUTOTUNE)
-    
-    return test_image
+
+    # Convert the image to RGB
+    image = image.convert("RGB")
+
+    # Resize the image
+    image = image.resize((width, height))
+
+    # Convert to NumPy array and normalize
+    image_array = np.array(image) / 255.0
+
+    # Expand dimensions (1, height, width, channels)
+    image_array = np.expand_dims(image_array, axis=0)
+
+
+    return image_array
